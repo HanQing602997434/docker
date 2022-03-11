a478fd883d5df6be839afb2c0dfa5eb0a1bc3ac5
hanqing <602997434@qq.com>
2022-03-10 22:15:17 -0800

    ubuntu docker

---
+++ ubuntu_docker.go
@@ -0,0 +1,13 @@
+
+// Ubuntu Docker安装
+/*
+	Docker Engine-Community 支持以下的Ubuntu版本
+		Xenial 16.04 (LTS)
+		Bionic 18.04 (LTS)
+		Cosmic 18.10
+		Disco 19.04
+		其他更新版本...
+
+	Docker Engine-Community 支持上 x86_64 (或amd64) armhf, arm64, s390x (IBM Z),
+	和ppc64le (IBM的Power) 架构。
+*/
