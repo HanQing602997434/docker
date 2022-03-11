3495e357998fc37b3133bdeb28165f674693783d
hanqing <602997434@qq.com>
2022-03-10 22:17:05 -0800

    ubuntu docker

--- ubuntu_docker.go
+++ ubuntu_docker.go
@@ -10,4 +10,11 @@
 
 	Docker Engine-Community 支持上 x86_64 (或amd64) armhf, arm64, s390x (IBM Z),
 	和ppc64le (IBM的Power) 架构。
+
+	使用官方安装脚本自动安装
+		安装命令：
+			curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+		也可以使用国内daocloud一键安装命令：
+			curl -sSL https://get.daocloud.io/docker | sh
 */
