f35c5aff5708241649460a08b2d2a3feac8fe1ec
hanqing <602997434@qq.com>
2022-03-11 01:38:14 -0800

    ubuntu docker

--- ubuntu_docker.go
+++ ubuntu_docker.go
@@ -17,4 +17,12 @@
 
 		也可以使用国内daocloud一键安装命令：
 			curl -sSL https://get.daocloud.io/docker | sh
+
+	手动安装
+		卸载旧版本
+			Docker的旧版本被称为docker，docker.io 或 docker-engine。如果已安装，请卸载它们：
+				$ sudo apt-get remove docker docker-engine docker.io containerd runc
+
+			当前称为Docker Engine-Community 软件包 docker-ce。
+			安装Docker Engine-Community，以下介绍两种方式。
 */
