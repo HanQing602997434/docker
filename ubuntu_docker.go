f84ef66498b0182a1df34f16e2fca413782ace57
hanqing <602997434@qq.com>
2022-03-13 22:47:07 -0700

    ubuntu docker

--- ubuntu_docker.go
+++ ubuntu_docker.go
@@ -74,5 +74,43 @@
   					docker-ce | 5:18.09.0~3-0~ubuntu-xenial | https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu  xenial/stable amd64 Packages
   					docker-ce | 18.06.1~ce~3-0~ubuntu       | https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu  xenial/stable amd64 Packages
   					docker-ce | 18.06.0~ce~3-0~ubuntu       | https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu  xenial/stable amd64 Packages
-  					...
+					...
+					  
+				使用第二列中的版本字符串安装特定版本，例如5:18.09.1~3-0~ubuntu-xenial
+					$ sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
+
+				测试Docker是否安装成功，输入以下命令，打印出以下信息则安装成功：
+					$ sudo docker run hello-world
+
+					Unable to find image 'hello-world:latest' locally
+					latest: Pulling from library/hello-world
+					1b930d010525: Pull complete                                                                                                                                  
+					Digest: sha256:c3b4ada4687bbaa170745b3e4dd8ac3f194ca95b2d0518b417fb47e5879d9b5f
+					Status: Downloaded newer image for hello-world:latest
+
+
+					Hello from Docker!
+					This message shows that your installation appears to be working correctly.
+
+
+					To generate this message, Docker took the following steps:
+ 						1. The Docker client contacted the Docker daemon.
+ 						2. The Docker daemon pulled the "hello-world" image from the Docker Hub.
+    					(amd64)
+ 						3. The Docker daemon created a new container from that image which runs the
+    					executable that produces the output you are currently reading.
+ 						4. The Docker daemon streamed that output to the Docker client, which sent it
+    					to your terminal.
+
+
+						To try something more ambitious, you can run an Ubuntu container with:
+ 						$ docker run -it ubuntu bash
+
+
+						Share images, automate workflows, and more with a free Docker ID:
+ 						https://hub.docker.com/
+
+
+						For more examples and ideas, visit:
+ 						https://docs.docker.com/get-started/
 */
