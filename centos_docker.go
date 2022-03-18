abb7a903d8beb1db116f6e13c733407a54fcded3
hanqing <602997434@qq.com>
2022-03-18 01:11:09 -0700

    centos_docker

--- centos_docker.go
+++ centos_docker.go
@@ -12,4 +12,17 @@
 
 		也可以使用国内daocloud一键安装命令：
 			curl -sSL https://get.daocloud.io/docker | sh
+
+	手动安装
+		卸载旧版本
+			较旧的Docker版本称为docker或docker-engine。如果已安装这些程序，请卸载它们以及
+			相关的依赖项。
+				$ sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
 */
