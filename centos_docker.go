b145dfa649cb51ec9be0f2fc7af0598ce39cc8aa
hanqing <602997434@qq.com>
2022-03-18 01:07:56 -0700

    centos_docker

---
+++ centos_docker.go
@@ -0,0 +1,15 @@
+
+// CentOS Docker安装
+/*
+	Docker支持以下的64位CentOS版本：
+		CentOS 7
+		CentOS 8
+		更高版本...
+
+	使用官方安装脚本自动安装
+		安装命令如下：
+			curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+		也可以使用国内daocloud一键安装命令：
+			curl -sSL https://get.daocloud.io/docker | sh
+*/
