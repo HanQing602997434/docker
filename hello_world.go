b1b37327ef8398a34780adefa2df7d728c0252cf
hanqing <602997434@qq.com>
2022-03-25 00:21:21 -0700

    hello world

--- hello_world.go
+++ hello_world.go
@@ -7,7 +7,12 @@
 		Hello world
 
 	各个参数解析：
-		docker: Docker的二进制执行文件
+		docker: Docker的二进制执行文件。
 
-		run: 与前面的 docker 组合来运行一个容器
+		run: 与前面的 docker 组合来运行一个容器。
+
+		ubuntu:15.10 指定要运行的镜像，Docker首先从本地主机上查找镜像是否存在，如果不存在，
+		Docker就会从镜像仓库 Docker Hub 下载公共镜像。
+
+		/bin/echo "Hello world": 在启动的容器里执行的命令。
 */
