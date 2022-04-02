cf6fd8be1e3fcfef4df0813b284fd3949aa94ff4
hanqing <602997434@qq.com>
2022-04-02 02:51:08 -0700

    docker容器使用

---
+++ docker_use.go
@@ -0,0 +1,11 @@
+
+// Docker 容器使用
+/*
+	Docker 客户端
+		docker客户端非常简单，我们可以直接输入 docker 命令来查看到 Docker 客户端的所有命令选项。
+			runoob@runoob:~# docker
+
+		可以通过命令 docker command --help更深入的了解指定的Docker命令使用方法。
+		例如我们要查看 docker status 指令的具体使用方法：
+			runoob@runoob:~# docker stats --help
+*/
