26ae40925a2824a72e9b1bd7462eee35790a92f5
hanqing <602997434@qq.com>
2022-05-12 23:13:12 -0700

    docker镜像

--- docker_image.go
+++ docker_image.go
@@ -3,4 +3,13 @@
 /*
 	当运行容器时，使用的镜像如果在本地不存在，docker就会自动从docker镜像仓库下载，默认是
 	从Docker Hub公共镜像源下载。
+	
+	列出镜像列表
+		我们可以使用docker images来列出本地主机上的镜像。
+		各个选项说明：
+			REPOSITORY：表示镜像的仓库源
+			TAG：镜像的标签
+			IMAGE ID：镜像ID
+			CREATED：镜像创建的时间
+			SIZE：镜像大小
 */
