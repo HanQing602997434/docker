be28733c357f8b64a5f34df7bbf99613158ba8e6
hq <602997434@qq.com>
2022-07-03 22:46:18 -0700

    docker仓库

--- docker_depot.go
+++ docker_depot.go
@@ -7,5 +7,9 @@
 		目前Docker官方维护了一个公共仓库Docker Hub。
 		大部分需求都可以通过在Docker Hub中直接下载镜像来实现。
 
-	
+	注册
+		在https://hub.docker.com免费注册一个Docker账号
+
+	登录和退出
+		登录需要输入用户名和密码，登录成功后，我们就可以从docker hub上拉取自己账号下的全部镜像。
 */
